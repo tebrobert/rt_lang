d5358cf4f07935aad16c2a463c6e5c991def8d4d
teb.robert <@tebrobert>
2022-12-31 23:10:56 +0300

    lexx almost tailrec

--- modules/lexx.py
+++ modules/lexx.py
@@ -1,4 +1,5 @@
 from modules.desugar import *
+from modules.tailrec import *
 
 class LexxErr(ValueError):
     def __init__(self, msg):
@@ -35,60 +36,96 @@ class Token_Eq_Gr:
 
 def lexx(code):
     end_of_code = '\0'
-    code += end_of_code
-    a = 0
-    b = 0
-    tokens = []
-    char = code[b]
 
-    while char != end_of_code:
-        if 'a' <= char <= 'z' or 'A' <= char <= 'Z' or char == "_":
-            a = b
-            while True:
-                b += 1
-                char = code[b]
-                if not('a' <= char <= 'z' or 'A' <= char <= 'Z' or '0' <= char <= '9' or char == '_'):
-                    break
+    @tailrec
+    def lexx_rec(code_ext, token_idx_end, tokens):
+        current_char = code_ext[token_idx_end]
 
-            tokens.append(Token_Idf(code[a:b]))
-    
-        elif char == '(':
-            tokens.append(Token_Paren_Open())
-            b += 1
+        if current_char == end_of_code:
+            return tokens
 
-        elif char == ')':
-            tokens.append(Token_Paren_Close())
-            b += 1
+        if 'a' <= current_char <= 'z' or 'A' <= current_char <= 'Z' or current_char == "_":
+            def get_idf_rec(code_ext, idx_idf_start, idf_current_idx):
+                current_idf_char = code_ext[idf_current_idx]
 
-        elif char == ')':
-            tokens.append(Token_Paren_Close())
-            b += 1
+                if not('a' <= current_idf_char <= 'z'
+                    or 'A' <= current_idf_char <= 'Z'
+                    or '0' <= current_idf_char <= '9'
+                    or current_idf_char == '_'
+                ):
+                    return rec(
+                        code_ext = code_ext,
+                        token_idx_end = idf_current_idx,
+                        tokens = tokens + [Token_Idf(code_ext[idx_idf_start:idf_current_idx])]
+                    )
 
-        elif char == '=':
-            char = code[b + 1]
-            if char == '>':
-                tokens.append(Token_Eq_Gr())
-                b += 2
+                return get_idf_rec(
+                    code_ext = code_ext,
+                    idx_idf_start = idx_idf_start,
+                    idf_current_idx = idf_current_idx + 1
+                )
 
-            else : raise LexxErr(f'Unexpected sequence "={char}"')
+            return get_idf_rec(
+                code_ext = code_ext,
+                idx_idf_start = token_idx_end,
+                idf_current_idx = token_idx_end
+            )
 
-        elif char == ' ':
-            b += 1
+        elif current_char == '(':
+            return rec(
+                code_ext = code_ext,
+                token_idx_end = token_idx_end + 1,
+                tokens = tokens + [Token_Paren_Open()]
+            )
 
-        elif char == '"':
-            b += 1
-            a = b
-            char = code[b]
-            while char != '"':
-                if char == end_of_code:
+        elif current_char == ')':
+            return rec(
+                code_ext = code_ext,
+                token_idx_end = token_idx_end + 1,
+                tokens = tokens + [Token_Paren_Close()]
+            )
+
+        elif current_char == '=':
+            next_char = code_ext[token_idx_end + 1]
+            if next_char == '>':
+                return rec(
+                    code_ext = code_ext,
+                    token_idx_end = token_idx_end + 2,
+                    tokens = tokens + [Token_Eq_Gr()]
+                )
+
+            else:
+                raise LexxErr(f'Unexpected sequence "={next_char}"')
+
+        elif current_char == ' ':
+            return rec(
+                code_ext = code_ext,
+                token_idx_end = token_idx_end + 1,
+                tokens = tokens
+            )
+
+        elif current_char == '"':
+            idx_string_end = token_idx_end + 1
+            idx_string_start = idx_string_end
+            current_string_char = code_ext[idx_string_end]
+
+            while current_string_char != '"':
+                if current_string_char == end_of_code:
                     raise LexxErr(f"No closing `\"` for string literal.")
-                b += 1
-                char = code[b]
-            tokens.append(Token_Lit_Str(code[a:b]))
-            b += 1
+                idx_string_end += 1
+                current_string_char = code_ext[idx_string_end]
 
-        else: raise LexxErr(f'Unexpected char "{char}"')
+            return rec(
+                code_ext = code_ext,
+                token_idx_end = idx_string_end + 1,
+                tokens = tokens + [Token_Lit_Str(code_ext[idx_string_start:idx_string_end])]
+            )
 
-        char = code[b]
+        else:
+            raise LexxErr(f'Unexpected current_char "{current_char}"')
 
-    return tokens
+    return lexx_rec(
+        code_ext = code + end_of_code,
+        token_idx_end = 0,
+        tokens = []
+    )
