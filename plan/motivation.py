a1a363e4d2655e9e5e5efa32a6b3edbca05209de
teb.robert <@tebrobert>
2023-01-01 13:08:44 +0300

    more motivation

--- plan/motivation.py
+++ plan/motivation.py
@@ -2,7 +2,11 @@
 no null pointer, effect types with requirements for better testability
 
 2. Simple static types with rememberable names (Vect, Struct)
+and also implement anonymous structs for more compiler awareness
 
-3. Simple lib importing and versioning (importing at a single file)
+3. To take responsibility for numeric types and values
+and make usage of values like inf and nan more clear
+
+4. Simple lib importing and versioning (importing at a single file)
 also it's possible to somehow use dates instead (or with) the versions numbers
 
