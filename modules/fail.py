c20d43b2a0b61b027ba0526daca0466e88b6580e
teb.robert <@tebrobert>
2023-01-01 12:31:01 +0300

    lexx is concise

---
+++ modules/fail.py
@@ -0,0 +1,2 @@
+def fail(ex):
+    raise ex
--- modules/lexx.py
+++ modules/lexx.py
@@ -1,3 +1,4 @@
+from modules.fail import *
 from modules.tailrec import *
 from modules.desugar import *
 
@@ -39,100 +40,62 @@ end_of_code = '\0'
 @tailrec
 def get_idx_string_end_rec(code_ext, idx_string_end):
     current_string_char = code_ext[idx_string_end]
-
-    if current_string_char == '"':
-        return idx_string_end
-
-    if current_string_char == end_of_code:
-        raise LexxErr(f"No closing `\"` for string literal.")
-
-    return rec(code_ext, idx_string_end + 1)
+    return (
+        idx_string_end
+            if current_string_char == '"' else
+        fail(LexxErr(f"No closing `\"` for string literal."))
+            if current_string_char == end_of_code else
+        rec(code_ext, idx_string_end + 1)
+    )
 
 @tailrec
 def get_idx_idf_end_rec(code_ext, idf_current_idx):
     current_idf_char = code_ext[idf_current_idx]
-
-    if not('a' <= current_idf_char <= 'z'
-        or 'A' <= current_idf_char <= 'Z'
-        or '0' <= current_idf_char <= '9'
-        or current_idf_char == '_'
-    ):
-        return idf_current_idx
-
-    return rec(
-        code_ext = code_ext,
-        idf_current_idx = idf_current_idx + 1
+    return (rec(code_ext, idf_current_idx + 1)
+        if ('a' <= current_idf_char <= 'z'
+            or 'A' <= current_idf_char <= 'Z'
+            or '0' <= current_idf_char <= '9'
+            or current_idf_char == '_'
+        )
+        else idf_current_idx
     )
+
+def lexx_idf(code_ext, tokens, token_idx_end):
+    idx_idf_start = token_idx_end
+    idx_idf_end = get_idx_idf_end_rec(code_ext, idx_idf_start + 1)
+    return (code_ext, idx_idf_end, tokens + [Token_Idf(code_ext[idx_idf_start:idx_idf_end])])
+
+def lexx_eq_gr(code_ext, tokens, token_idx_end):
+    return ((code_ext, token_idx_end + 2, tokens + [Token_Eq_Gr()])
+        if code_ext[token_idx_end + 1] == '>'
+        else fail(LexxErr(f'Unexpected sequence "={code_ext[token_idx_end + 1]}"'))
+    )
+
+def lexx_string(code_ext, tokens, token_idx_end):
+    idx_string_start = token_idx_end + 1
+    idx_string_end = get_idx_string_end_rec(code_ext, idx_string_start)
+    return (code_ext, idx_string_end + 1, tokens + [Token_Lit_Str(code_ext[idx_string_start:idx_string_end])])
 
 @tailrec
 def lexx_rec(code_ext, token_idx_end, tokens):
-    current_char = code_ext[token_idx_end]
-
-    if current_char == end_of_code:
-        return tokens
-
-    if 'a' <= current_char <= 'z' or 'A' <= current_char <= 'Z' or current_char == "_":
-        idx_idf_start = token_idx_end
-        idx_idf_end = get_idx_idf_end_rec(
-            code_ext = code_ext,
-            idf_current_idx = idx_idf_start + 1
-        )
-
-        return rec(
-            code_ext = code_ext,
-            token_idx_end = idx_idf_end,
-            tokens = tokens + [Token_Idf(code_ext[idx_idf_start:idx_idf_end])]
-        )
-
-    elif current_char == '(':
-        return rec(
-            code_ext = code_ext,
-            token_idx_end = token_idx_end + 1,
-            tokens = tokens + [Token_Paren_Open()]
-        )
-
-    elif current_char == ')':
-        return rec(
-            code_ext = code_ext,
-            token_idx_end = token_idx_end + 1,
-            tokens = tokens + [Token_Paren_Close()]
-        )
-
-    elif current_char == '=':
-        next_char = code_ext[token_idx_end + 1]
-        if next_char == '>':
-            return rec(
-                code_ext = code_ext,
-                token_idx_end = token_idx_end + 2,
-                tokens = tokens + [Token_Eq_Gr()]
-            )
-
-        else:
-            raise LexxErr(f'Unexpected sequence "={next_char}"')
-
-    elif current_char == ' ':
-        return rec(
-            code_ext = code_ext,
-            token_idx_end = token_idx_end + 1,
-            tokens = tokens
-        )
-
-    elif current_char == '"':
-        idx_string_start = token_idx_end + 1
-        idx_string_end = get_idx_string_end_rec(code_ext, idx_string_start)
-        
-        return rec(
-            code_ext = code_ext,
-            token_idx_end = idx_string_end + 1,
-            tokens = tokens + [Token_Lit_Str(code_ext[idx_string_start:idx_string_end])]
-        )
-
-    else:
-        raise LexxErr(f'Unexpected current_char "{current_char}"')
+    current_char = code_ext[token_idx_end]    
+    return (
+        tokens
+            if current_char == end_of_code else
+        rec(*lexx_idf(code_ext, tokens, token_idx_end))
+            if 'a' <= current_char <= 'z' or 'A' <= current_char <= 'Z' or current_char == "_" else
+        rec(code_ext, token_idx_end + 1, tokens + [Token_Paren_Open()])
+            if current_char == '(' else
+        rec(code_ext, token_idx_end + 1, tokens + [Token_Paren_Close()])
+            if current_char == ')' else
+        rec(*lexx_eq_gr(code_ext, tokens, token_idx_end))
+            if current_char == '=' else
+        rec(code_ext, token_idx_end + 1, tokens)
+            if current_char == ' ' else
+        rec(*lexx_string(code_ext, tokens, token_idx_end))
+            if current_char == '"' else
+        fail(LexxErr(f'Unexpected current_char "{current_char}"'))
+    )
 
 def lexx(code):
-    return lexx_rec(
-        code_ext = code + end_of_code,
-        token_idx_end = 0,
-        tokens = []
-    )
+    return lexx_rec(code + end_of_code, 0, [])
