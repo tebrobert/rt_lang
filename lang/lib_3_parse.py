06e86e65da51e5535fd11372ef7ba2ab848bb5fd
Robert Tebiev <RKTebiev@sberbank.ru>
2023-01-30 00:04:22 +0300

    parse horizontal limit

--- lang/lib_3_parse.py
+++ lang/lib_3_parse.py
@@ -35,7 +35,9 @@ class Expr_Call_1:
 class Expr_Lambda_1:
     def __init__(self, eidf_x, expr_res):
         if type(eidf_x) is not Expr_Idf:
-            fail(ParseErr(f'Expr_Idf expected as the first arg of Expr_Lambda_1'))
+            fail(ParseErr(
+                f'Expr_Idf expected as the first arg of Expr_Lambda_1'
+            ))
 
         self.eidf_x, self.expr_res = eidf_x, expr_res
 
@@ -49,54 +51,53 @@ class Expr_Lambda_1:
 def parse_lit_str(tokens, i):
     if type(tokens[i]) is not Token_Lit_Str:
         return fail(ParseErr(f'Token_Lit_Str expected given {i} {tokens}'))
-
     return Expr_Lit_Str(tokens[i].s), i + 1
 
 def parseIdf(tokens, i):
     if type(tokens[i]) is not Token_Idf:
         return fail(ParseErr(f'Token_Idf expected given {i} {tokens}'))
-
     return Expr_Idf(tokens[i].s), i + 1
 
 def parseBraced(tokens, i):
     if type(tokens[i]) is not Token_Paren_Open:
-        return fail(ParseErr(f'Token_Paren_Open: expected given {i} {tokens}'))
-
+        return fail(ParseErr(
+            f'Token_Paren_Open: expected given {i} {tokens}'
+        ))
     expr, j = parseExpr(tokens, i + 1)
-
     if type(tokens[j]) is not Token_Paren_Close:
-        return fail(ParseErr(f'Token_Paren_Open expected at {j} given {i} {tokens}'))
-
+        return fail(ParseErr(
+            f'Token_Paren_Open expected at {j} given {i} {tokens}'
+        ))
     return expr, j + 1
 
 def parseLambda1(tokens, i):
     eidf_x, j = parseIdf(tokens, i)
-
     if type(tokens[j]) is not Token_Eq_Gr:
-        return fail(ParseErr(f'Token_Eq_Gr expected at {j} given {i} {tokens}'))
-
+        return fail(ParseErr(
+            f'Token_Eq_Gr expected at {j} given {i} {tokens}'
+        ))
     expr_res, k = parseExpr(tokens, j + 1)
     return Expr_Lambda_1(eidf_x, expr_res), k
 
 @tailrec
 def parseCall(tokens, expr_f, i):
     if type(tokens[i]) is not Token_Paren_Open:
-        return fail(ParseErr(f'Token_Paren_Open: expected, given {i} {tokens}'))
-
+        return fail(ParseErr(
+            f'Token_Paren_Open: expected, given {i} {tokens}'
+        ))
     if type(tokens[i+1]) is Token_Paren_Close:
-        return fail(ParseErr(f'Remove deprecated empty parenthesis, given {i} {tokens}'))
-
+        return fail(ParseErr(
+            f'Remove deprecated empty parenthesis, given {i} {tokens}'
+        ))
     expr_x, j = parseExpr(tokens, i + 1)
-
     if type(tokens[j]) is not Token_Paren_Close:
-        return fail(ParseErr(f'Token_Paren_Open expected at {j} given {i} {tokens}'))
-
+        return fail(ParseErr(
+            f'Token_Paren_Open expected at {j} given {i} {tokens}'
+        ))
     k = j + 1
     expr_call_1 = Expr_Call_1(expr_f, expr_x)
-
     if type(tokens[k]) is Token_Paren_Open:
         return rec(tokens, expr_call_1, k)
-
     return expr_call_1, k
 
 @tailrec
@@ -116,7 +117,9 @@ def parseAnyOf(tokens, i, parsers):
     return expr, j
 
 def parseExpr(tokens, i):
-    return parseAnyOf(tokens, i, [parseLambda1, parseIdf, parseBraced, parse_lit_str])
+    return parseAnyOf(tokens, i,
+        [parseLambda1, parseIdf, parseBraced, parse_lit_str]
+    )
 
 def parse(tokens):
     end_of_tokens = '\0'
