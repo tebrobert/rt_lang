458d43ed796de5f710412a3a52bfd3d816d4f1b0
Robert Tebiev <RKTebiev@sberbank.ru>
2023-01-30 00:00:34 +0300

    lexx horizontal limit

--- lang/lib_2_lexx.py
+++ lang/lib_2_lexx.py
@@ -61,27 +61,36 @@ def get_idx_idf_end_rec(code_ext, idf_current_idx):
 def lexx_idf(code_ext, tokens, token_idx_end):
     idx_idf_start = token_idx_end
     idx_idf_end = get_idx_idf_end_rec(code_ext, idx_idf_start + 1)
-    return (code_ext, idx_idf_end, tokens + [Token_Idf(code_ext[idx_idf_start:idx_idf_end])])
+    return (code_ext, idx_idf_end,
+        tokens + [Token_Idf(code_ext[idx_idf_start:idx_idf_end])]
+    )
 
 def lexx_eq_gr(code_ext, tokens, token_idx_end):
     return ((code_ext, token_idx_end + 2, tokens + [Token_Eq_Gr()])
         if code_ext[token_idx_end + 1] == '>'
-        else fail(LexxErr(f'Unexpected sequence "={code_ext[token_idx_end + 1]}"'))
+        else fail(LexxErr(
+            f'Unexpected sequence "={code_ext[token_idx_end + 1]}"'
+        ))
     )
 
 def lexx_string(code_ext, tokens, token_idx_end):
     idx_string_start = token_idx_end + 1
     idx_string_end = get_idx_string_end_rec(code_ext, idx_string_start)
-    return (code_ext, idx_string_end + 1, tokens + [Token_Lit_Str(code_ext[idx_string_start:idx_string_end])])
+    return (code_ext, idx_string_end + 1,
+        tokens + [Token_Lit_Str(code_ext[idx_string_start:idx_string_end])]
+    )
 
 @tailrec
 def lexx_rec(code_ext, token_idx_end, tokens):
-    current_char = code_ext[token_idx_end]    
+    current_char = code_ext[token_idx_end]
     return (
         tokens
             if current_char == end_of_code else
         rec(*lexx_idf(code_ext, tokens, token_idx_end))
-            if 'a' <= current_char <= 'z' or 'A' <= current_char <= 'Z' or current_char == "_" else
+            if ('a' <= current_char <= 'z'
+                or 'A' <= current_char <= 'Z'
+                or current_char == "_"
+            ) else
         rec(code_ext, token_idx_end + 1, tokens + [Token_Paren_Open()])
             if current_char == '(' else
         rec(code_ext, token_idx_end + 1, tokens + [Token_Paren_Close()])
