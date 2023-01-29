a0dc4670673b740c78f9d139790c28c922be69af
Robert Tebiev <RKTebiev@sberbank.ru>
2023-01-29 05:33:54 +0300

    some warns fixed

--- rt.py
+++ rt.py
@@ -3,15 +3,21 @@ import os
 from lang._6_run import *
 
 def readFile(filePath):
-    with open(filePath) as f: return f.read()
+    with open(filePath) as f:
+        return f.read()
 
-class ArgParser():
-    def __init__(self): self.argParser = argparse.ArgumentParser()
-    def add(self, x, **kwargs): self.argParser.add_argument(x, **kwargs); return self
-    def parse(self): return self.argParser.parse_args()
+class ArgParser:
+    def __init__(self):
+        self.argParser = argparse.ArgumentParser()
+    def add(self, x, **kwargs):
+        self.argParser.add_argument(x, **kwargs)
+        return self
+    def parse(self):
+        return self.argParser.parse_args()
 
 def unsafeRunCode(code, dev):
-    if dev: print(f'1_CODE:\n{code}')
+    if dev:
+        print(f'1_CODE:\n{code}')
     try:
         desugared = desugar(code); print(f"2_DESUGARED:\n{desugared}\n" if dev else "", end="")
         tokens = lexx(desugared); print(f"3_TOKENS:\n{tokens}\n" if dev else "", end="")
@@ -20,10 +26,16 @@ def unsafeRunCode(code, dev):
         shown = show(typed); print(f"6_SHOWN:\n{shown}\n" if dev else "", end="")
         compiled = compile(shown); print("7_RUNNING:\n" if dev else "", end="")
         unsafeRunCompiled(compiled)
-    except Exception as e: print(e.msg)
+    except Exception as e:
+        print(e)
 
 def main():
-    args = ArgParser().add("code", nargs="?").add("--dev", action="store_true").add("--test", action="store_true").parse()
+    args = (ArgParser()
+        .add("code", nargs="?")
+        .add("--dev", action="store_true")
+        .add("--test", action="store_true")
+        .parse()
+    )
 
     if args.test:
         pathTests = 'tests/'
@@ -33,20 +45,35 @@ def main():
         path4Expr      = '4_expr.py'
         path5Typed     = '5_typed.txt'
         path6Shown     = '6_shown.py'
-        for currentElement in os.listdir(pathTests):
+        for currentElement in sorted(os.listdir(pathTests)):
             pathCurrentTest = f"{pathTests}{currentElement}/"
             print(currentElement, end=": ")
             try:
                 code = readFile(f"{pathCurrentTest}/{path1Code}")
-                desugared = desugar(code); assert desugared == readFile(f"{pathCurrentTest}/{path2Desugared}"), f"!=desugared, got: {desugared}"
-                tokens = lexx(desugared); assert str(tokens) == readFile(f"{pathCurrentTest}/{path3Tokens}"), f"!=tokens, got: {tokens}"
-                expr = parse(tokens); assert str(expr) == readFile(f"{pathCurrentTest}/{path4Expr}"), f"!=expr, got: {expr}"
-                typed = sem(expr); assert str(typed) == readFile(f"{pathCurrentTest}/{path5Typed}"), f"!=typed, got: {typed}"
-                shown = show(typed); assert shown == readFile(f"{pathCurrentTest}/{path6Shown}"), f"!=shown, got: {shown}"
-                compiled = compile(shown)
-                print("PASSED")
-            except AssertionError as e: print("FAILED: "+str(e))
-            except Exception as e: print("FAILED: "+str(e))
-    elif args.code is not None: unsafeRunCode(code=readFile(args.code), dev=args.dev)
 
-if __name__ == "__main__": main()
+                desugared = desugar(code)
+                assert desugared == readFile(f"{pathCurrentTest}/{path2Desugared}"), f"!=desugared, got: {desugared}"
+
+                tokens = lexx(desugared)
+                assert str(tokens) == readFile(f"{pathCurrentTest}/{path3Tokens}"), f"!=tokens, got: {tokens}"
+
+                expr = parse(tokens)
+                assert str(expr) == readFile(f"{pathCurrentTest}/{path4Expr}"), f"!=expr, got: {expr}"
+
+                typed = sem(expr)
+                assert str(typed) == readFile(f"{pathCurrentTest}/{path5Typed}"), f"!=typed, got: {typed}"
+
+                shown = show(typed)
+                assert shown == readFile(f"{pathCurrentTest}/{path6Shown}"), f"!=shown, got: {shown}"
+
+                _ = compile(shown)
+                print("PASSED")
+            except AssertionError as e:
+                print("FAILED: "+str(e))
+            except Exception as e:
+                print("FAILED: "+str(e))
+    elif args.code is not None:
+        unsafeRunCode(code=readFile(args.code), dev=args.dev)
+
+if __name__ == "__main__":
+    main()
