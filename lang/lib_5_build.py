c6dbf4ab28c28b8841455e808f2aa4c56d09a0f2
Robert Tebiev <RKTebiev@sberbank.ru>
2023-01-30 00:12:08 +0300

    vertical and horizontal limits for 5, 6

--- lang/lib_5_build.py
+++ lang/lib_5_build.py
@@ -11,10 +11,8 @@ class CallableShowableLambda_1:
     def __init__(self, f, s):
         if not callable(f):
             fail(ValueError("f should be callable"))
-
         if not type(s) is str:
             fail(ValueError("s should be a string"))
-
         self.s, self.f = s, f
 
     def __call__(self, arg):
@@ -47,59 +45,51 @@ class Flatmap:
     def __init__(self, a_fb, fa):
         if not callable(a_fb):
             fail(ValueError("a_fb should be callable"))
-
         if not fa.runnable():
-            fail(ValueError(f"fa should be runnable but it is {fa=}, also {a_fb=}"))
-
+            fail(ValueError(
+                f"fa should be runnable but it is {fa=}, also {a_fb=}"
+            ))
         self.a_fb, self.fa = a_fb, fa
 
     def __repr__(self):
         if not type(self.a_fb) is CallableShowableLambda_1:
             fail(BuildErr("Cannot show lambda"))
-
         return f'Flatmap({self.a_fb}, {self.fa})'
 
     def runnable(self):
         return True
 
-def show(typed, lamb_arg_stack=[]):
+def showTypedLit(tlit):
+    if tlit.typ == T_Str:
+        return f"\"{tlit.s}\""
+    return fail(BuildErr(f"Unexpected literal `{tlit}`"))
+
+def showTypedIdf(tidf, lambArgStack):
+    if tidf.s == builtin_input:
+        return "Input()"
+    if tidf.s == builtin_print:
+        return "(lambda s: Print(s))"
+    if tidf.s == builtin_flatmap:
+        return "(lambda a_fb: lambda fa: Flatmap(a_fb, fa))"
+    if tidf.s in lambArgStack:
+        return tidf.s
+    return fail(BuildErr(f"Unexpected identifier `{tidf.s}`"))
+
+def show(typed, lambArgStack=[]):
     if type(typed) is Typed_Lit:
-        tlit = typed
-
-        if tlit.typ == T_Str:
-            return f"\"{tlit.s}\""
-
-        return fail(BuildErr(f"Unexpected literal `{tlit}`"))
-
+        return showTypedLit(typed)
     if type(typed) is Typed_Idf:
-        tidf = typed
-
-        if tidf.s == builtin_input:
-            return "Input()"
-
-        if tidf.s == builtin_print:
-            return "(lambda s: Print(s))"
-
-        if tidf.s == builtin_flatmap:
-            return "(lambda a_fb: lambda fa: Flatmap(a_fb, fa))"
-
-        if tidf.s in lamb_arg_stack:
-            return tidf.s
-
-        return fail(BuildErr(f"Unexpected identifier `{tidf.s}`"))
-
+        return showTypedIdf(typed, lambArgStack)
     if type(typed) is Typed_Call_1:
         tcall = typed
-        shown_f = show(tcall.typed_f, lamb_arg_stack)
-        shown_x = show(tcall.typed_x, lamb_arg_stack)
+        shown_f = show(tcall.typed_f, lambArgStack)
+        shown_x = show(tcall.typed_x, lambArgStack)
         return f"({shown_f})({shown_x})"
-    
     if type(typed) is Typed_Lambda_1:
         tlamb = typed
         s = tlamb.tidf_x.s
-        return f"(lambda {s}: {show(tlamb.typed_res, [s]+lamb_arg_stack)})"
-    
-    return fail(BuildErr(f"Unexpected typed expression {typed}"))
+        return f"(lambda {s}: {show(tlamb.typed_res, [s] + lambArgStack)})"
+    fail(BuildErr(f"Unexpected typed expression {typed}"))
 
 def build(shown):
     return eval(shown)
