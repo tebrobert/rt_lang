cf44ec80117d63eb5aa0fbadc8b91174e747ef25
Robert <@tebrobert>
2023-10-01 16:42:48 +0300

    decomposing

--- lang/lib_4_sem.py
+++ lang/lib_4_sem.py
@@ -135,6 +135,24 @@ def solve_unknown_0(
     )
 
 
+def solve_type_1(
+    typ_f, typ_x,
+    typ_sub_fx, typ_sub_x,
+    f_x_synched_unks, synched_unks,
+):
+    if type(typ_sub_x) is Unknown0:
+        if typ_sub_x.s in f_x_synched_unks:
+            return fail(
+                f"Can't match the types #remember the case A=>A vs A=>{builtin_List}[A]")
+        return fail(
+            f"Yet can't call {typ_f} with {typ_x} currently matching {typ_sub_fx} and {typ_sub_x}"
+        )
+    if type(typ_sub_x) is Type1:
+        return solve_rec(typ_sub_fx.t1, typ_sub_x.t1,
+            (typ_f, typ_x, synched_unks))
+    return fail(f"Can't match the types {typ_sub_fx} vs {typ_sub_x}")
+
+
 def solve_rec(typ_sub_fx, typ_sub_x, f_x_synched_unks):
     typ_f, typ_x, synched_unks = f_x_synched_unks
 
@@ -153,17 +171,11 @@ def solve_rec(typ_sub_fx, typ_sub_x, f_x_synched_unks):
         )
 
     if type(typ_sub_fx) is Type1:
-        if type(typ_sub_x) is Unknown0:
-            if typ_sub_x.s in f_x_synched_unks:
-                return fail(
-                    f"Can't match the types #remember the case A=>A vs A=>{builtin_List}[A]")
-            return fail(
-                f"Yet can't call {typ_f} with {typ_x} currently matching {typ_sub_fx} and {typ_sub_x}"
-            )
-        if type(typ_sub_x) is Type1:
-            return solve_rec(typ_sub_fx.t1, typ_sub_x.t1,
-                (typ_f, typ_x, synched_unks))
-        return fail(f"Can't match the types {typ_sub_fx} vs {typ_sub_x}")
+        return solve_type_1(
+            typ_f, typ_x,
+            typ_sub_fx, typ_sub_x,
+            f_x_synched_unks, synched_unks,
+        )
 
     if type(typ_sub_fx) is Type2:
         if type(typ_sub_x) is Unknown0:
