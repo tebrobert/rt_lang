363bb2c152761c6305a7183d3f96941ba27e825e
Robert <@tebrobert>
2023-10-24 19:37:09 +0300

    test

--- tests/custom.py
+++ tests/custom.py
@@ -2,36 +2,36 @@ from lang.lib_5_build import *
 from utils.fail import *
 
 
-def method_syntax_1():
+def test_method_syntax_1():
     rt_assert(
         full_parse("a.+(b).+(c).+(d)") ==
         full_parse("+(d)(+(c)(+(b)(a)))")
     )
 
 
-def method_syntax_2():
+def test_method_syntax_2():
     rt_assert(
         full_parse("f0(r0)(l0).f1(r1)(l1).f2(r2)(l2)") ==
         full_parse("f2(r2)(l2)(f1(r1)(l1)(f0(r0)(l0)))")
     )
 
 
-def test_assign():
+def test_assignment():
     full_build("""msg = "hi"\nprint(msg)""")
 
 
-def test_assign_lambdas_1():
+def test_assignment_lambdas_1():
     full_build("""f1 <- pure(+("1"))\nprint("0".f1)""")
 
 
-def test_assign_lambdas_2():
+def test_assignment_lambdas_2():
     full_build("""f1 = x => x.+("1")\nprint("0".f1)""")
 
 
 custom_tests = [
-    test_assign,
-    method_syntax_1,
-    method_syntax_2,
-    test_assign_lambdas_1,
-    test_assign_lambdas_2,
+    test_assignment,
+    test_assignment_lambdas_1,
+    test_assignment_lambdas_2,
+    test_method_syntax_1,
+    test_method_syntax_2,
 ]
