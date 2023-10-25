80db49313d85ecf42eedbbc9ea175d70a39ca3d3
Robert <@tebrobert>
2023-10-25 10:08:30 +0300

    .

--- tests/custom.py
+++ tests/custom.py
@@ -18,14 +18,11 @@ def test_assignment_lambdas():
     full_build("""f1 = x => x.+("1")\nprint("0".f1)""")
 
 
-def test_method_syntax_1():
+def test_method_syntax():
     rt_assert(
         full_parse("a.+(b).+(c).+(d)") ==
         full_parse("+(d)(+(c)(+(b)(a)))")
     )
-
-
-def test_method_syntax_2():
     rt_assert(
         full_parse("f0(r0)(l0).f1(r1)(l1).f2(r2)(l2)") ==
         full_parse("f2(r2)(l2)(f1(r1)(l1)(f0(r0)(l0)))")
@@ -36,8 +33,7 @@ custom_tests = [
     test_sync_typs,
 
     test_assignment,
-    test_method_syntax_1,
-    test_method_syntax_2,
+    test_method_syntax,
 ]
 
 deferred_tests = [
