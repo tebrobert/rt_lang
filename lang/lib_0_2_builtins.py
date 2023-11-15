737dd572c5279db7bc24a1346e88b9af80a21d9e
Robert <@tebrobert>
2023-11-15 16:08:24 +0300

    .

--- lang/lib_0_2_builtins.py
+++ lang/lib_0_2_builtins.py
@@ -8,7 +8,7 @@ def T_List(t1): return Typ1(builtin_List, t1)
 def T_RIO(t1): return Typ1(builtin_RIO, t1)
 def T_Func(t1, t2): return Typ2(builtin_Func, t1, t2)
 
-T_A0 = Unk0(1)
+T_A0 = Unk0(0)
 T_A1 = Unk0(2)
 
 types = {
