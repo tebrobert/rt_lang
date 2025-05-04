39746c373bd6522ce4c9c6dcdcf048b347d5c4ea
Robert <@tebrobert>
2025-05-05 00:09:21 +0400

    ...

--- src/test/scala/org/rt/allTests/TestCase1.scala
+++ src/test/scala/org/rt/allTests/TestCase1.scala
@@ -6,5 +6,5 @@ import org.rt.RtTestCase
 object TestCase1 extends RtTestCase {
   val code_0 = "input"
   val tokens_1 = List(TokenIdf("input"))
-  val expr_2 = lang.RtLib_3_Parse.ExprIdf("???")
+  val expr_2 = lang.RtLib_3_Parse.ExprIdf("input")
 }
