b82f0d4208cce1a703b086ab3d3bef503babb56d
Robert <@tebrobert>
2025-08-12 14:52:59 -0200

    .

--- src/test/scala/org/rt/allTests/TestCase4.scala
+++ src/test/scala/org/rt/allTests/TestCase4.scala
@@ -1,8 +1,10 @@
 package org.rt.allTests
 
 import org.rt.RtTestCase
+import org.rt.lang.RtLib_0_2_Builtins.{T_Func, T_RIO, T_Str}
 import org.rt.lang.RtLib_2_Tokenize.*
 import org.rt.lang.RtLib_3_Parse.*
+import org.rt.lang.RtLib_4_Typify.{TypifiedCall1, TypifiedIdf, TypifiedLambda1}
 
 object TestCase4 extends RtTestCase {
   val code_0 = "(s => s)((s => s)(input))\n"
@@ -23,4 +25,22 @@ object TestCase4 extends RtTestCase {
         ExprIdf("input")
       )
     )
+
+  override val mb_typified_3 = Some(TypifiedCall1(
+    TypifiedLambda1.create(
+      TypifiedIdf("s", T_RIO(T_Str)),
+      TypifiedIdf("s", T_RIO(T_Str)),
+      T_Func(T_RIO(T_Str), T_RIO(T_Str)),
+    ),
+    TypifiedCall1(
+      TypifiedLambda1.create(
+        TypifiedIdf("s", T_RIO(T_Str)),
+        TypifiedIdf("s", T_RIO(T_Str)),
+        T_Func(T_RIO(T_Str), T_RIO(T_Str)),
+      ),
+      TypifiedIdf("input", T_RIO(T_Str)),
+      T_RIO(T_Str),
+    ),
+    T_RIO(T_Str),
+  ))
 }
