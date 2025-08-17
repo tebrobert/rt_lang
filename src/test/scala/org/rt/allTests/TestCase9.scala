74516a79255de392273b3b94222b3788476a7172
Robert <@tebrobert>
2025-08-17 19:21:48 -0200

    .

--- src/test/scala/org/rt/allTests/TestCase9.scala
+++ src/test/scala/org/rt/allTests/TestCase9.scala
@@ -5,19 +5,21 @@ import org.rt.lang.RtLib_2_Tokenize.Classes.*
 import org.rt.lang.RtLib_3_Parse.*
 
 object TestCase9 extends RtTestCase {
-  val code_0 = ""
-    + s"""print("Hey! What is your name?")\n"""
-    + s"""name <- input\n"""
-    + s"""print("Welcome, ...")\n"""
-    + s"""print(name)\n"""
+  val code_0 =
+    """print("Hey! What is your name?")
+      |name <- input
+      |print("Welcome, ...")
+      |print(name)
+      |""".stripMargin
 
-  val tokens_1 = List(
-    TokIdf("print"), TokParenOpen,
-    TokLitStr("Hey! What is your name?"), TokParenClose, TokEndl,
-    TokIdf("name"), TokLessMinus, TokIdf("input"), TokEndl,
-    TokIdf("print"), TokParenOpen, TokLitStr("Welcome, ..."), TokParenClose, TokEndl,
-    TokIdf("print"), TokParenOpen, TokIdf("name"), TokParenClose, TokEndl,
-  )
+  val tokens_1 =
+    List(
+      TokIdf("print"), TokParenOpen,
+      TokLitStr("Hey! What is your name?"), TokParenClose, TokEndl,
+      TokIdf("name"), TokLessMinus, TokIdf("input"), TokEndl,
+      TokIdf("print"), TokParenOpen, TokLitStr("Welcome, ..."), TokParenClose, TokEndl,
+      TokIdf("print"), TokParenOpen, TokIdf("name"), TokParenClose, TokEndl,
+    )
 
   val expr_2 =
     ExprCall1(
