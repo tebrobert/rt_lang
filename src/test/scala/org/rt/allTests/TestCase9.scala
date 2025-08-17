19f340cb93f4d3e2e8b426905ceb722ea6615588
Robert <@tebrobert>
2025-08-17 19:25:44 -0200

    .

--- src/test/scala/org/rt/allTests/TestCase9.scala
+++ src/test/scala/org/rt/allTests/TestCase9.scala
@@ -1,6 +1,7 @@
 package org.rt.allTests
 
 import org.rt.RtTestCase
+import org.rt.TestHelpers.exprAndThen
 import org.rt.lang.RtLib_2_Tokenize.Classes.*
 import org.rt.lang.RtLib_3_Parse.*
 
@@ -14,49 +15,18 @@ object TestCase9 extends RtTestCase {
 
   val tokens_1 =
     List(
-      TokIdf("print"), TokParenOpen,
-      TokLitStr("Hey! What is your name?"), TokParenClose, TokEndl,
+      TokIdf("print"), TokParenOpen, TokLitStr("Hey! What is your name?"), TokParenClose, TokEndl,
       TokIdf("name"), TokLessMinus, TokIdf("input"), TokEndl,
       TokIdf("print"), TokParenOpen, TokLitStr("Welcome, ..."), TokParenClose, TokEndl,
       TokIdf("print"), TokParenOpen, TokIdf("name"), TokParenClose, TokEndl,
     )
 
   val expr_2 =
-    ExprCall1(
-      ExprCall1(
-        ExprIdf(">>="),
-        ExprLambda1(
-          ExprIdf("_"),
-          ExprCall1(
-            ExprCall1(
-              ExprIdf(">>="),
-              ExprLambda1(
-                ExprIdf("name"),
-                ExprCall1(
-                  ExprCall1(
-                    ExprIdf(">>="),
-                    ExprLambda1(
-                      ExprIdf("_"),
-                      ExprCall1(
-                        ExprIdf("print"),
-                        ExprIdf("name")
-                      )
-                    )
-                  ),
-                  ExprCall1(
-                    ExprIdf("print"),
-                    ExprLitStr("Welcome, ...")
-                  )
-                )
-              )
-            ),
-            ExprIdf("input")
-          )
+    exprAndThen("_", ExprCall1(ExprIdf("print"), ExprLitStr("Hey! What is your name?")),
+      exprAndThen("name", ExprIdf("input"),
+        exprAndThen("_", ExprCall1(ExprIdf("print"), ExprLitStr("Welcome, ...")),
+          ExprCall1(ExprIdf("print"), ExprIdf("name"))
         )
-      ),
-      ExprCall1(
-        ExprIdf("print"),
-        ExprLitStr("Hey! What is your name?")
       )
     )
 }
