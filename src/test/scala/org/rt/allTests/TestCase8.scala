3753e0d00c9ce33b509416abdbe8d01afb8fe6d8
Robert <@tebrobert>
2025-08-17 18:27:20 -0200

    .

--- src/test/scala/org/rt/allTests/TestCase8.scala
+++ src/test/scala/org/rt/allTests/TestCase8.scala
@@ -1,8 +1,10 @@
 package org.rt.allTests
 
 import org.rt.RtTestCase
+import org.rt.lang.RtLib_0_2_Builtins.{T_A0, T_Func, T_RIO, T_Str, T_Unit}
 import org.rt.lang.RtLib_2_Tokenize.*
 import org.rt.lang.RtLib_3_Parse.*
+import org.rt.lang.RtLib_4_Typify.{TypifiedCall1, TypifiedIdf, TypifiedLambda1}
 
 object TestCase8 extends RtTestCase {
   val code_0 = "s <- input\n"
@@ -41,4 +43,40 @@ object TestCase8 extends RtTestCase {
       ),
       ExprIdf("input")
     )
+
+  override val mb_typified_3 = Some(
+    TypifiedCall1(
+      TypifiedCall1(
+        TypifiedIdf(">>=", T_Func(T_Func(T_Str, T_RIO(T_Unit)), T_Func(T_RIO(T_Str), T_RIO(T_Unit)))),
+        TypifiedLambda1(
+          TypifiedIdf("s", T_Str),
+          TypifiedCall1(
+            TypifiedCall1(
+              TypifiedIdf(">>=", T_Func(T_Func(T_A0, T_RIO(T_Unit)), T_Func(T_RIO(T_Unit), T_RIO(T_Unit)))),
+              TypifiedLambda1(
+                TypifiedIdf("_", T_A0),
+                TypifiedCall1(
+                  TypifiedIdf("print", T_Func(T_Str, T_RIO(T_Unit))),
+                  TypifiedIdf("s", T_Str),
+                  T_RIO(T_Unit)
+                ),
+                T_Func(T_A0, T_RIO(T_Unit))
+              ),
+              T_Func(T_RIO(T_Unit), T_RIO(T_Unit))
+            ),
+            TypifiedCall1(
+              TypifiedIdf("print", T_Func(T_Str, T_RIO(T_Unit))),
+              TypifiedIdf("s", T_Str),
+              T_RIO(T_Unit)
+            ),
+            T_RIO(T_Unit)
+          ),
+          T_Func(T_Str, T_RIO(T_Unit))
+        ),
+        T_Func(T_RIO(T_Str), T_RIO(T_Unit))
+      ),
+      TypifiedIdf("input", T_RIO(T_Str)),
+      T_RIO(T_Unit)
+    )
+  )
 }
