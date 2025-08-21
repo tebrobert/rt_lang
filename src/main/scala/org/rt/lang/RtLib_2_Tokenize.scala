910d15947439090d547cf06481e86cae96b9a981
Robert <@tebrobert>
2025-08-21 15:18:05 -0200

    .

--- src/main/scala/org/rt/lang/RtLib_2_Tokenize.scala
+++ src/main/scala/org/rt/lang/RtLib_2_Tokenize.scala
@@ -278,8 +278,7 @@ object RtLib_2_Tokenize {
         tokens
       else if (current_char == ' ')
         tokenize_rec((code_ext, current_idx + 1, tokens))
-      else
-        tokenize_rec(tokenize_first_of(lexxBundle)(all_tokenizers))
+      else tokenize_rec(tokenize_first_of(lexxBundle)(all_tokenizers))
     }
   }
 
