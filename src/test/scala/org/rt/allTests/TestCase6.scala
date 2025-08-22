e8fae499314b58df770cb383353031f2dd457b9c
Robert <@tebrobert>
2025-08-22 12:30:17 -0200

    .

--- src/test/scala/org/rt/allTests/TestCase6.scala
+++ src/test/scala/org/rt/allTests/TestCase6.scala
@@ -46,37 +46,40 @@ object TestCase6 extends RtTestCase {
       ExprIdf("input")
     )
 
-  override val mb_linted_3 = Some(LintedCall1(
-    LintedCall1(
-      LintedIdf(">>=", (T_Str tTo T_RIO(T_Unit)) tTo (T_RIO(T_Str) tTo T_RIO(T_Unit))),
-      LintedLambda1(
-        LintedIdf("s", T_Str),
+  override val mb_linted_3 =
+    Some(
+      LintedCall1(
         LintedCall1(
-          LintedCall1(
-            LintedIdf(">>=", (T_Unit tTo T_RIO(T_Unit)) tTo (T_RIO(T_Unit) tTo T_RIO(T_Unit))),
-            LintedLambda1(
-              LintedIdf("u", T_Unit),
+          LintedIdf(">>=", (T_Str tTo T_RIO(T_Unit)) tTo (T_RIO(T_Str) tTo T_RIO(T_Unit))),
+          LintedLambda1(
+            LintedIdf("s", T_Str),
+            LintedCall1(
+              LintedCall1(
+                LintedIdf(">>=", (T_Unit tTo T_RIO(T_Unit)) tTo (T_RIO(T_Unit) tTo T_RIO(T_Unit))),
+                LintedLambda1(
+                  LintedIdf("u", T_Unit),
+                  LintedCall1(
+                    LintedIdf("print", T_Str tTo T_RIO(T_Unit)),
+                    LintedIdf("s", T_Str),
+                    T_RIO(T_Unit)
+                  ),
+                  T_Unit tTo T_RIO(T_Unit),
+                ),
+                T_RIO(T_Unit) tTo T_RIO(T_Unit),
+              ),
               LintedCall1(
                 LintedIdf("print", T_Str tTo T_RIO(T_Unit)),
                 LintedIdf("s", T_Str),
                 T_RIO(T_Unit)
               ),
-              T_Unit tTo T_RIO(T_Unit),
+              T_RIO(T_Unit)
             ),
-            T_RIO(T_Unit) tTo T_RIO(T_Unit),
+            T_Str tTo T_RIO(T_Unit),
           ),
-          LintedCall1(
-            LintedIdf("print", T_Str tTo T_RIO(T_Unit)),
-            LintedIdf("s", T_Str),
-            T_RIO(T_Unit)
-          ),
-          T_RIO(T_Unit)
+          T_RIO(T_Str) tTo T_RIO(T_Unit),
         ),
-        T_Str tTo T_RIO(T_Unit),
-      ),
-      T_RIO(T_Str) tTo T_RIO(T_Unit),
-    ),
-    LintedIdf("input", T_RIO(T_Str)),
-    T_RIO(T_Unit)
-  ))
+        LintedIdf("input", T_RIO(T_Str)),
+        T_RIO(T_Unit)
+      )
+    )
 }
