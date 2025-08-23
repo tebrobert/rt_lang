4e69cb0ebaf58b8e28985972dfc9446cbda4af3b
Robert <@tebrobert>
2025-08-23 10:47:36 -0200

    .

--- src/test/scala/org/rt/allTests/TestCase11.scala
+++ src/test/scala/org/rt/allTests/TestCase11.scala
@@ -1,8 +1,11 @@
 package org.rt.allTests
 
 import org.rt.RtTestCase
+import org.rt.TestHelpers.*
+import org.rt.lang.RtLib_0_2_Builtins.*
 import org.rt.lang.RtLib_2_Tokenize.Public.*
 import org.rt.lang.RtLib_3_Parse.*
+import org.rt.lang.RtLib_4_Lint.*
 
 object TestCase11 extends RtTestCase {
   val code_0 =
@@ -23,53 +26,26 @@ object TestCase11 extends RtTestCase {
     )
 
   val expr_2 =
-    ExprCall1(
-      ExprCall1(
-        ExprIdf(">>="),
-        ExprLambda1(
-          ExprIdf("greeting"),
-          ExprCall1(
-            ExprCall1(
-              ExprIdf(">>="),
-              ExprLambda1(
-                ExprIdf("_"),
-                ExprCall1(
-                  ExprCall1(
-                    ExprIdf(">>="),
-                    ExprLambda1(
-                      ExprIdf("name"),
-                      ExprCall1(
-                        ExprCall1(
-                          ExprIdf(">>="),
-                          ExprLambda1(
-                            ExprIdf("_"),
-                            ExprCall1(
-                              ExprIdf("print"),
-                              ExprIdf("name")
-                            )
-                          )
-                        ),
-                        ExprCall1(
-                          ExprIdf("print"),
-                          ExprLitStr("Welcome, ...")
-                        )
-                      )
-                    )
-                  ),
-                  ExprIdf("input")
-                )
-              )
-            ),
-            ExprCall1(
-              ExprIdf("print"),
-              ExprIdf("greeting")
-            )
-          )
-        )
+    exprAndThen("greeting", ExprCall1(ExprIdf("pure"), ExprLitStr("Hey! What is your name?")),
+      exprAndThen("_", ExprCall1(ExprIdf("print"), ExprIdf("greeting")),
+        exprAndThen("name", ExprIdf("input"),
+          exprAndThen("_", ExprCall1(ExprIdf("print"), ExprLitStr("Welcome, ...")),
+            ExprCall1(ExprIdf("print"), ExprIdf("name")),
+          ),
+        ),
       ),
-      ExprCall1(
-        ExprIdf("pure"),
-        ExprLitStr("Hey! What is your name?")
+    )
+
+  override val mb_linted_3 =
+    Some(
+      lintedAndThen(("greeting", T_Str), LintedCall1(LintedIdf("pure", T_Str_To_RIO_Str), LintedLit("Hey! What is your name?", T_Str), T_RIO_Str),
+        lintedAndThen(("_", T_Unit), LintedCall1(LintedIdf("print", T_Str_To_RIO_Unit), LintedIdf("greeting", T_Str), T_RIO_Unit),
+          lintedAndThen(("name", T_Str), LintedIdf("input", T_RIO_Str),
+            lintedAndThen(("_", T_Unit), LintedCall1(LintedIdf("print", T_Str_To_RIO_Unit), LintedLit("Welcome, ...", T_Str), T_RIO_Unit),
+              LintedCall1(LintedIdf("print", T_Str_To_RIO_Unit), LintedIdf("name", T_Str), T_RIO_Unit),
+            ),
+          ),
+        ),
       )
     )
 }
