1216e5a9e6777ebd179e0c57b17a1148e6b94c2a
Robert <@tebrobert>
2025-08-23 11:08:49 -0200

    .

--- src/test/scala/org/rt/TestHelpers.scala
+++ src/test/scala/org/rt/TestHelpers.scala
@@ -6,6 +6,7 @@ import org.rt.lang.RtLib_0_2_Builtins.*
 import org.rt.lang.RtLib_3_Parse.*
 import org.rt.lang.RtLib_4_Lint.*
 import org.rt.utils.RtFail.rt_assert_type_Typ1
+import org.rt.utils.RtList.rtMatch
 
 object TestHelpers {
   val T_RIO_Unit = T_RIO(T_Unit)
@@ -13,19 +14,25 @@ object TestHelpers {
   val T_Str_To_RIO_Unit = T_Str tTo T_RIO_Unit
   val T_Str_To_RIO_Str = T_Str tTo T_RIO_Str
 
+  @scala.annotation.tailrec
+  def exprCurrCall(
+    f: Expr,
+    xs: Expr*,
+  ): Expr =
+    xs.toList.rtMatch(
+      caseEmpty = () => f,
+      caseAtLeast1 = (headX, tailX) =>
+        exprCurrCall(ExprCall1(f, headX), tailX:_*),
+    )
+
   def exprAndThen(
     result: String,
     expr: Expr,
     exprNext: Expr,
   ) =
-    ExprCall1(
-      ExprCall1(
-        ExprIdf(builtin_flatmap),
-        ExprLambda1(
-          ExprIdf(result),
-          exprNext,
-        )
-      ),
+    exprCurrCall(
+      ExprIdf(builtin_flatmap),
+      ExprLambda1(ExprIdf(result), exprNext),
       expr,
     )
 
