57e87fb4a2420ce7144df9c701c7792aee5bd740
Robert <@tebrobert>
2025-08-23 18:49:52 -0200

    ..

--- src/main/scala/org/rt/lang/RtLib_5_Build.scala
+++ src/main/scala/org/rt/lang/RtLib_5_Build.scala
@@ -5,7 +5,7 @@ import org.rt.lang.RtLib_0_1_Types.*
 import org.rt.lang.RtLib_0_2_Builtins.*
 import org.rt.lang.RtLib_4_Lint.{Linted, LintedCall1, LintedIdf, LintedLambda1, LintedLit}
 import org.rt.lang.RtLib_5_Build.Public.*
-import org.rt.utils.RtFail.{rtFail, wip}
+import org.rt.utils.RtFail.{rtFail, rt_try, wip}
 
 object RtLib_5_Build {
   object Public {
@@ -26,29 +26,29 @@ object RtLib_5_Build {
     type Bint = BigInt
     type Bool = Boolean
 
-    sealed trait Built
+    sealed trait BuiltSc
 
-    sealed trait BuiltLit extends Built
+    sealed trait BuiltScLit extends BuiltSc // Sc - Scala
 
-    case object BuiltUnit extends BuiltLit
+    case object BuiltScLitUnit extends BuiltScLit
 
-    case class BuiltStr(s: Str) extends BuiltLit
+    case class BuiltScLitStr(s: Str) extends BuiltScLit
 
-    case class BuiltBint(i: Bint) extends BuiltLit
+    case class BuiltScLitBint(i: Bint) extends BuiltScLit
 
-    case class BuiltBool(b: Bool) extends BuiltLit
+    case class BuiltScLitBool(b: Bool) extends BuiltScLit
 
-    case class BuiltFunc(
-      f: (Unit => Built)
-        | (Str => Built)
-        | (Bint => Built)
-        | (Bool => Built)
-        | (BuiltFunc => Built)
-    ) extends Built
+    case class BuiltScLamb(
+      f: (Unit => BuiltSc)
+        | (Str => BuiltSc)
+        | (Bint => BuiltSc)
+        | (Bool => BuiltSc)
+        | (BuiltScLamb => BuiltSc)
+    ) extends BuiltSc
 
     def buildScala[A](
       typed: Linted,
-    ): Built =
+    ): BuiltSc =
       Internal.buildScalaWithStacks(
         typed = typed,
         lamb_arg_stack = List.empty, // legacy todo remove
@@ -59,6 +59,27 @@ object RtLib_5_Build {
   }
 
   private object Internal {
+    def buildScLitBint(
+      serializedValue: String
+    ) = {
+      val bint = rt_try(() => BigInt(serializedValue))
+        .getOrElse(rtFail(s"Unexpected BigInt: `$serializedValue`."))
+
+      BuiltScLitBint(bint)
+    }
+
+
+    def buildScLit(
+      serializedValue: String,
+      typ: Typ,
+    ): BuiltScLit = {
+      if (typ == T_Str)
+        BuiltScLitStr(serializedValue)
+      else if (typ == T_Bint)
+        buildScLitBint(serializedValue)
+      else rtFail(s"Unexpected literal: `$serializedValue`, `$typ`.")
+    }
+
     def buildScalaLambda_1(
       t_idf_x: LintedIdf,
       typed_res: Linted,
@@ -66,19 +87,19 @@ object RtLib_5_Build {
       lambArgStackStr: List[Str], // Lists will most likely turn into dicts Map[String, _]
       lambArgStackBint: List[Bint],
       lambArgStackBool: List[Bool],
-    ): BuiltFunc =
+    ): BuiltScLamb =
       t_idf_x.typ match {
-        case T_Unit => BuiltFunc((_: Unit) =>
+        case T_Unit => BuiltScLamb((_: Unit) =>
           //todo - beautify stacks
           buildScalaWithStacks(typed_res, lamb_arg_stack, lambArgStackStr, lambArgStackBint, lambArgStackBool)
         )
-        case T_Str => BuiltFunc((s: Str) =>
+        case T_Str => BuiltScLamb((s: Str) =>
           buildScalaWithStacks(typed_res, lamb_arg_stack, s +: lambArgStackStr, lambArgStackBint, lambArgStackBool)
         )
-        case T_Bint => BuiltFunc((i: Bint) =>
+        case T_Bint => BuiltScLamb((i: Bint) =>
           buildScalaWithStacks(typed_res, lamb_arg_stack, lambArgStackStr, i +: lambArgStackBint, lambArgStackBool)
         )
-        case T_Bool => BuiltFunc((b: Bool) =>
+        case T_Bool => BuiltScLamb((b: Bool) =>
           buildScalaWithStacks(typed_res, lamb_arg_stack, lambArgStackStr, lambArgStackBint, b +: lambArgStackBool)
         )
 
@@ -93,7 +114,7 @@ object RtLib_5_Build {
       lambArgStackStr: List[Str],
       lambArgStackBint: List[Bint],
       lambArgStackBool: List[Bool],
-    ): Built =
+    ): BuiltSc =
       typed match {
         case LintedLit(s, typ) => ???
         case LintedIdf(s, typ) => ???
