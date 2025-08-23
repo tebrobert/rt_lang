2718f910a0d7a0dcb57b0210bd50212edd5e3c2f
Robert <@tebrobert>
2025-08-23 18:51:26 -0200

    ..

--- src/main/scala/org/rt/lang/RtLib_5_Build.scala
+++ src/main/scala/org/rt/lang/RtLib_5_Build.scala
@@ -116,7 +116,7 @@ object RtLib_5_Build {
       lambArgStackBool: List[Bool],
     ): BuiltSc =
       typed match {
-        case LintedLit(s, typ) => ???
+        case LintedLit(s, typ) => buildScLit(s, typ)
         case LintedIdf(s, typ) => ???
         case LintedCall1(linted_f, linted_x, typ) => ???
         case LintedLambda1(linted_idf_x, linted_res, typ) =>
