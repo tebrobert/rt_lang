c3d713a0ce2eeabf011106c49224a206b9c9f82d
Robert <@tebrobert>
2025-09-05 12:33:22 -0200

    .......

--- src/main/scala/org/rt/Main.scala
+++ src/main/scala/org/rt/Main.scala
@@ -1,6 +1,8 @@
 package org.rt
 
 import org.rt.lang.RtLib_2_Tokenize.Public.tokenize
+import org.rt.lang.RtLib_5_Build.Public.{Built, BuiltBrick, fullBuildScala}
+import org.rt.utils.RtFail.rtFail
 import zio.*
 import zio.Console.printLine
 
@@ -9,5 +11,26 @@ import java.io.IOException
 object Main extends ZIOAppDefault:
 
   override def run: ZIO[ZIOAppArgs, IOException, Unit] = {
-    ZIO.succeed(org.rt.lang.RtLib_5_Build.test8)
+    ZIO.succeed{
+      def run(
+        built: Built,
+      ): Unit =
+        built match {
+          case BuiltBrick(run) => run()
+          case _ => rtFail("runtime")
+        }
+
+      println("HERE START")
+
+      val b = fullBuildScala(
+        """s <- input
+          |print(s)
+          |print(s)
+          |""".stripMargin
+      )
+      run(b)
+
+      println("HERE FINISH")
+      ()
+    }
   }
