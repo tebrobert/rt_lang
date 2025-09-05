d823e6a43df5fc0de991c3900d8f608c8a596ec8
Robert <@tebrobert>
2025-09-05 13:03:38 -0200

    .......

--- src/test/scala/org/rt/TestsRunner.scala
+++ src/test/scala/org/rt/TestsRunner.scala
@@ -13,7 +13,7 @@ trait RtTestCase {
   val tokens_1: List[Tok]
   val expr_2: Expr
   val linted_3: Linted
-  val mb_mock_4: Option[List[RunMock]]
+  val mb_mock_4: Option[List[RunMock]] = None
 }
 
 object TestsRunner extends ZIOSpecDefault {
