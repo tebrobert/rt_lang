45b7117063980221ca01b7504350c100b75f875f
Robert <@tebrobert>
2025-09-13 11:38:39 -0200

    .

--- src/main/scala/org/rt/Main.scala
+++ src/main/scala/org/rt/Main.scala
@@ -1,40 +1,26 @@
 package org.rt
 
-import org.rt.lang.RtLib_1_Tokenize.Public.tokenize
-import org.rt.lang.RtLib_4_Build.Public
-import org.rt.lang.RtLib_4_Build.Public.{Brick, BrickInput, BrickPrint, Built, BuiltRio, BuiltStr, BuiltUnit, fullBuild}
-import org.rt.utils.RtFail.{RtFail, rtFail}
+import org.rt.lang.RtLib_4_Build.Public.*
+import org.rt.lang.RtLib_5_Run.liveBrickRunner
 import zio.*
-import zio.Console.printLine
 
 import java.io.IOException
 
 object Main extends ZIOAppDefault:
 
   override def run: ZIO[ZIOAppArgs, IOException, Unit] = {
-    for {
-      _ <- ZIO.unit
-
-      brickRunner = (brick: Brick) =>
-          brick match {
-            case BrickInput => BuiltRio(ZIO.succeed(BuiltStr(scala.io.StdIn.readLine())))
-            case BrickPrint(s) => BuiltRio(ZIO.succeed(BuiltUnit(println(s))))
-          }
-
-      code =
-        """nameRequest = "What is your name?"
-          |print(nameRequest)
-          |name <- input
-          |print("Dear " + name + ", welcome!")
-          |""".stripMargin
-
-      b = fullBuild(code, brickRunner)
-      _ <- b match {
-        case Left(fail) => ZIO.succeed(println(fail))
-        case Right(built) => org.rt.lang.RtLib_5_Run.run(built)
-          .catchAll {
-            fail => ZIO.succeed(println(fail))
-          }
-      }
-    } yield ()
+    fullBuild(
+      """nameRequest = "What is your name?"
+        |print(nameRequest)
+        |name <- input
+        |print("Dear " + name + ", welcome!")
+        |""".stripMargin,
+      liveBrickRunner
+    ) match {
+      case Left(fail) => ZIO.succeed(println(fail))
+      case Right(built) => org.rt.lang.RtLib_5_Run.run(built).unit
+        .catchAll {
+          fail => ZIO.succeed(println(fail))
+        }
+    }
   }
