0b99a5d73c19088302f25c12699b9d44c26d4a08
Robert <@tebrobert>
2025-09-14 09:35:48 -0200

    .

---
+++ src/main/scala/org/rt/MainWeb.scala
@@ -0,0 +1,12 @@
+package org.rt
+
+import zio.*
+
+import java.io.IOException
+
+object MainWeb extends ZIOAppDefault:
+
+  override def run: ZIO[ZIOAppArgs, IOException, Unit] =
+    ???
+
+  // todo - design api
