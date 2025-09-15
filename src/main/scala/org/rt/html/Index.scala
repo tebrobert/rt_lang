c36624b39da55a5100bdd8c9f491ff10856dc5e3
Robert <@tebrobert>
2025-09-15 15:43:03 -0200

    .

--- src/main/scala/org/rt/html/Index.scala
+++ src/main/scala/org/rt/html/Index.scala
@@ -2,95 +2,49 @@ package org.rt.html
 
 object Index {
   val page = """
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Three Text Areas Layout with Buttons</title>
-  <style>
-    html, body {
-      height: 100%;
-      margin: 0;
-    }
-
-    .container {
-      display: flex;
-      height: 98vh;
-    }
-
-    .left {
-      width: 45%;
-      padding: 10px;
-      box-sizing: border-box;
-      display: flex;
-      flex-direction: column;
-    }
-
-    .right {
-      width: 45%;
-      display: flex;
-      flex-direction: column;
-    }
-
-    .top-right {
-      height: 64vh;
-      padding: 10px;
-      box-sizing: border-box;
-    }
-
-    .middle-right {
-      height: 5vh;
-      padding: 10px;
-      box-sizing: border-box;
-    }
-
-    .bottom-right {
-      height: 20vh;
-      padding: 10px;
-      box-sizing: border-box;
-    }
-
-    textarea {
-      width: 100%;
-      height: 100%;
-      resize: none;
-      font-size: 20pt;
-      font-family: 'Courier New', Courier, monospace;
-    }
-
-    button {
-      margin-top: 10px;
-      padding: 10px;
-    }
-  </style>
-</head>
-<body>
-  <div class="container">
-    <div class="left">
-      <label for="left-textarea">Your code</label>
-      <textarea>
-message = "What is your name?"
-print(message)
-name &lt- input
-print("Dear " + name + ", welcome!")
-</textarea>
-      <button>Run</button>
-    </div>
-    <div class="right">
-      <div class="top-right">
-        <label for="left-textarea">Console</label>
-        <textarea readonly></textarea>
+<html>
+  <head>
+    <title>RT Lang</title>
+    <style>
+      html, body, div, textarea { width: 100%; height: 100%; }
+      textarea { resize: none; font-size: 20pt; font-family: 'Courier New', Courier, monospace; }
+      button { padding:10px; width:45%; background-color:darkblue; color:lightgray}
+    </style>
+  </head>
+  <body style="background-color:gray;">
+    <div class="container" style="display:flex; height:98%;">
+      <div style="width:45%; padding:10px; box-sizing:border-box; display:flex; flex-direction:column;">
+        <div style="height: 90%; padding:10px; box-sizing:border-box;">
+          <b style="color:darkred;">Your code</b>
+          <textarea style="color:darkblue; background-color:white;">message = "What is your name?"&#10;print(message)&#10;name &lt- input&#10;print("Dear " + name + ", welcome!")&#10;</textarea>
+        </div>
+        <div style="height:1%; padding:10px; box-sizing:border-box;">
+        </div>
+        <div style="height:4%; padding:10px; box-sizing:border-box;">
+          <button>Run</button>
+        </div>
       </div>
-      <div class="middle-right">
-      </div>
-      <div class="bottom-right">
-        <label for="left-textarea">Input</label>
-        <textarea></textarea>
-        <button>Enter</button>
+      <div style="width:45%; padding:10px; box-sizing:border-box; display:flex; flex-direction:column;">
+        <div style="height: 90%; padding:10px; box-sizing:border-box; display:flex; flex-direction:column;">
+          <div style="height: 75%; display:flex; flex-direction:column;">
+            <b style="color:darkred;">Console</b>
+            <textarea readonly style="background-color:black; color:lime;">sd</textarea>
+          </div>
+          <div style="height:5%; display:flex; flex-direction:column;">
+          </div>
+          <div style="height: 20%;">
+            <b style="color: darkred;">Input</b>
+            <textarea style="background-color:black; color:lime;">sd</textarea>
+          </div>
+        </div>
+        <div style="height:1%; padding:10px; box-sizing:border-box;">
+        </div>
+        <div style="height:4%; padding:10px; box-sizing:border-box;">
+          <button>Enter</button>
+        </div>
       </div>
     </div>
-  </div>
-</body>
+  </body>
 </html>
 """
 }
